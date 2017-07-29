7a0cd95183b65b251ee303a904cd69deb8650685
Mordokay <papajorgioster@gmail.com>
2017-07-29 17:14:29 +0100

    Initial Content Commit

---
+++ Assets/Scripts/MapTerrainGenerator.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapTerrainGenerator : MonoBehaviour {
+
+    public int sizeX;
+    public int sizeY;
+
+    public Noise.NormalizeMode normalizedMode;
+    [Tooltip("Generates a map based on a number")]
+    public int seed = 1234;
+    public Vector2 offset;
+    public float noiseScale;
+    public int octaves;
+    [Range(0, 1)]
+    public float persistence;
+    public float lacunarity;
+    public int[ , ] mapHeight;
+
+
+    public bool hideObjects;
+
+    public Vector2 mapCenter = Vector2.zero;
+
+    float[,] noiseMap;
+
+    [System.Serializable]
+    public class NoiseElement{
+
+        [SerializeField]
+        public GameObject myObject;
+        [SerializeField]
+        public float minTheshhold;
+        [SerializeField]
+        public float maxTheshold;
+        [SerializeField]
+        public float upShift;
+    }
+
+    [SerializeField]
+    public List<NoiseElement> myItems;
+
+    void Reset()
+    {
+        sizeX = 40;
+        sizeY = 40;
+        seed = 12345;
+        noiseScale = 5;
+        octaves = 2;
+        persistence = 0.56f;
+        lacunarity = 0;
+        mapCenter = Vector2.zero;
+        offset = Vector2.zero;
+        normalizedMode = Noise.NormalizeMode.Global;
+    }
+
+    void Start()
+    {
+        seed = PlayerPrefs.GetInt("seed");
+        CreateHeight();
+    }
+
+    void OnInspectorGUI()
+    {
+        Debug.Log("Changed something!!!");
+        //CreateHeight();
+    }
+
+    void CreateHeight()
+    {
+        mapHeight = new int[sizeX, sizeY];
+
+        noiseMap = Noise.GeneratedNoiseMap(sizeX, sizeY, seed, noiseScale, octaves, persistence, lacunarity, mapCenter + offset, normalizedMode);
+
+        for(int i = -sizeX / 2; i <= sizeX / 2 ; i++)
+        {
+            for (int j = -sizeY / 2; j <= sizeY / 2; i++)
+            {
+                float greyColor = noiseMap[i, j];
+                //Instanciate  object
+                foreach (NoiseElement el in myItems)
+                {
+                    if (greyColor > el.minTheshhold && greyColor < el.maxTheshold)
+                    {
+                        GameObject myObj = Instantiate(el.myObject) as GameObject;
+                        myObj.transform.position = new Vector3(i , 0.0f, j);
+                        myObj.transform.Translate(Vector3.up * el.upShift);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Noise{
+	
+    public enum NormalizeMode { Local, Global};
+
+    public static float[,] GeneratedNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistence, float lacunarity, Vector2 offset, NormalizeMode normalizeMode)
+    {
+
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+
+        float maxPossibleHeight = 0;
+        float amplitude = 1;
+        float frequency = 1;
+
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-10000, 10000) + offset.x;
+            float offsetY = prng.Next(-10000, 10000) - offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+
+            maxPossibleHeight += amplitude;
+            amplitude *= persistence;
+        }
+
+
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        if(scale <= 0){ scale = 0.001f; }
+
+        float maxLocalNoiseheight = float.MinValue;
+        float minLocalNoiseheight = float.MaxValue;
+
+        float halfWidth = mapWidth / 2f;
+        float halfHeight = mapHeight / 2f;
+
+        for (int y = 0; y < mapHeight; y++){
+            for(int x = 0; x < mapWidth; x++){
+
+                amplitude = 1;
+                frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+
+                    float sampleX = (x - halfWidth + octaveOffsets[i].x) / scale * frequency ;
+                    float sampleY = (y - halfHeight + octaveOffsets[i].y) / scale * frequency;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistence;
+                    frequency *= lacunarity;
+                }
+
+                if(noiseHeight > maxLocalNoiseheight)
+                {
+                    maxLocalNoiseheight = noiseHeight;
+                }else if (noiseHeight < minLocalNoiseheight)
+                {
+                    minLocalNoiseheight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                if (normalizeMode == NormalizeMode.Local)
+                {
+                    noiseMap[x, y] = Mathf.InverseLerp(minLocalNoiseheight, maxLocalNoiseheight, noiseMap[x, y]);
+                }
+                else
+                {
+                    float normalizedHeight = (noiseMap[x, y] + 1) / (2f * maxPossibleHeight / 2.0f);
+                    noiseMap[x, y] = Mathf.Clamp( normalizedHeight, 0 , int.MaxValue);
+                }
+            }
+        }
+        return noiseMap;
+    }
+}
