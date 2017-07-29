fbe4b021f3e8599c9211495996ae160b9ef110dc
Mordokay <papajorgioster@gmail.com>
2017-07-29 20:45:45 +0100

    Ranged Shooting

--- Assets/Scripts/PlayerAtackController.cs
+++ Assets/Scripts/PlayerAtackController.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerAtackController : MonoBehaviour {
 
-	// Use this for initialization
+    public GameObject bullet;
+    public float bulletSpeed;
+
 	void Start () {
-		
-	}
+        bulletSpeed = 2.0f;
+    }
 	
-	// Update is called once per frame
+	
 	void Update () {
-		
-	}
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                GameObject myBullet = Instantiate(bullet, this.transform.position, transform.rotation);
+                Vector3 direction = hit.point - myBullet.transform.position;
+                direction.y = 0;
+                myBullet.GetComponent<Rigidbody>().velocity = direction * bulletSpeed;
+            }
+        }
+    }
 }
