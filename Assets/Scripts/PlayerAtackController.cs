55c30c17bc875a324801d34b458db5f3359ccfb3
Mordokay <papajorgioster@gmail.com>
2017-07-30 00:19:48 +0100

    Tweeking Stuff. beggining of NavMesh (Still not working)

--- Assets/Scripts/PlayerAtackController.cs
+++ Assets/Scripts/PlayerAtackController.cs
@@ -6,14 +6,15 @@ public class PlayerAtackController : MonoBehaviour {
 
     public GameObject bullet;
     public float bulletSpeed;
+    GameObject gm;
 
 	void Start () {
         bulletSpeed = 2.0f;
+        gm = GameObject.FindGameObjectWithTag("GameManager");
     }
 	
-	
 	void Update () {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && !gm.GetComponent<PlayerStats>().droppingBeacon)
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
