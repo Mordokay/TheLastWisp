f60717a52f85acb69697f64279a0329201786be7
Mordokay <papajorgioster@gmail.com>
2017-07-29 19:59:04 +0100

    More stuff

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour {
+	
+	void Start () {
+        transform.rotation = Random.rotation;
+    }
+}
