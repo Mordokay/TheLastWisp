0df793839c75f69eafa56f4acc390e5b92f3edca
António Machado <machado.antonio.91@gmail.com>
2017-07-30 06:15:20 +0100

    Health

--- Assets/Scripts/PlayerAtackController.cs
+++ Assets/Scripts/PlayerAtackController.cs
@@ -6,25 +6,33 @@ public class PlayerAtackController : MonoBehaviour {
 
     public GameObject bullet;
     public float bulletSpeed;
+    PlayerStats stats;
     GameObject gm;
 
 	void Start () {
-        bulletSpeed = 2.0f;
         gm = GameObject.FindGameObjectWithTag("GameManager");
+        stats = gm.GetComponent<PlayerStats>();
     }
 	
 	void Update () {
-        if (Input.GetButtonDown("Fire1") && !gm.GetComponent<PlayerStats>().droppingBeacon)
+        if (Input.GetButtonDown("Fire1") && !stats.droppingBeacon && !stats.isLowHealth())
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
                 GameObject myBullet = Instantiate(bullet, this.transform.position, transform.rotation);
-                Vector3 direction = hit.point - myBullet.transform.position;
+                Vector3 direction = (hit.point - myBullet.transform.position).normalized;
                 direction.y = 0;
                 myBullet.GetComponent<Rigidbody>().velocity = direction * bulletSpeed;
             }
+
+            stats.LoseHealth(7.5f);
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+
         }
     }
 }
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,15 +5,48 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour {
 
     public bool droppingBeacon = true;
+    public float lightDecreaseSpeed;
+    Light playerHealth;
     GameObject player;
+    bool isLosingHealth;
+    float finalHealth;
+    bool isGainingHealth;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.gameObject.GetComponentInChildren<Light>();
+        finalHealth = playerHealth.spotAngle;
     }
 
     private void Update()
     {
+        if(finalHealth!= playerHealth.spotAngle)
+        {
+            if(finalHealth < playerHealth.spotAngle)
+            {
+                isLosingHealth = true;
+                isGainingHealth = false;
+            }
+            else {
+                isGainingHealth = true;
+                isLosingHealth = false;
+            }
+        }
+
+        if (isLosingHealth)
+        {
+            if(playerHealth.spotAngle <= finalHealth)
+            {
+                playerHealth.spotAngle = finalHealth;
+                isLosingHealth = false;
+            }
+            else
+            {
+                playerHealth.spotAngle -= Time.deltaTime * lightDecreaseSpeed;
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             if (droppingBeacon)
@@ -25,6 +58,25 @@ public class PlayerStats : MonoBehaviour {
             {
                 droppingBeacon = true;
             }
+        }
+    }
+
+    public bool isLowHealth()
+    {
+        return finalHealth <= 30;
+    }
+
+    public void LoseHealth(float amount)
+    {
+        finalHealth  -= amount;
+    }
+
+    public void GainHealth(float amount)
+    {
+        finalHealth += amount;
+        if(finalHealth > 110)
+        {
+            finalHealth = 110;
         }
     }
 }
